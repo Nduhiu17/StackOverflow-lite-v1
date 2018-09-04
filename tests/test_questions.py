87ff20d6b9f43445d9d619758377e27c2c395633
Your Name <nduhiu254@gmail.com>
2018-09-04 13:31:39 +0300

    Add and test endpoint to post a question

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -24,3 +24,27 @@ class TestQuestion(unittest.TestCase):
                                      body="how to init python how to init python how to init python")
         self.assertTrue(type(self.new_question.id), int)
         self.assertEqual(type(self.new_question), Question)
+
+    def test_question_posted(self):
+            # method to test a question can be posted
+        new_question = {'title': 'error sit voluptatem accusantium doloremque laudantium',
+                        'body': 'error sit voluptatem accusantium doloremque laudantiumerror sit volupta'}
+        response = self.client.post('api/v1/questions', data=json.dumps(new_question),
+                                    headers={'Content-Type': 'application' '/json'})
+        self.assertEqual(response.status_code, 201)
+
+    def test__post_invalid_title(self):
+            # test cant post with an invalid title
+        new_question = {'title': 'shotitle',
+                        'body': 'error sit voluptatem accusantium doloremque laudantiumerror sit voluptatem accusantium doloremque laudantium'}
+        response = self.client.post('api/v1/questions', data=json.dumps(new_question),
+                                    headers={'Content-Type': 'application' '/json'})
+        self.assertEqual(response.status_code, 400)
+
+    def test_post_invalid_body(self):
+        # test cant post with an invalid body
+        new_question = {'title': 'error sit voluptatem accusantium doloremque laudantium',
+                        'body': 'short body'}
+        response = self.client.post('api/v1/questions', data=json.dumps(new_question),
+                                    headers={'Content-Type': 'application' '/json'})
+        self.assertEqual(response.status_code, 400)
