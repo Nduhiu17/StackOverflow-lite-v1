491d2d770d42e71db930ac5a35e0be957452ff87
Your Name <nduhiu254@gmail.com>
2018-09-14 23:55:47 +0300

    Improve pylint ratings by checking coding pylint violated rules

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, Blueprint#importing flask
 from flask_restplus import Api
-
-from config import DevelopmentConfig
 from flask_jwt_extended import JWTManager
+from config import DevelopmentConfig
+
 
 authorization = {
     'apiKey': {
@@ -12,7 +12,6 @@ authorization = {
     }
 }
 
-
 app = Flask(__name__)
 jwt = JWTManager(app)
 app.config.from_object(DevelopmentConfig)
@@ -20,16 +19,9 @@ blueprint = Blueprint('api', __name__)
 blueprint_2 = Blueprint('home', __name__)
 blueprint_3 = Blueprint('auth', __name__)
 
-api_v1 = Api(blueprint,
-             title='StackOverflow - Lite',
-             version='1',
-             description='An api to create a question, create an answer to a question ,get all questions and get a single '
-                         'question with its answers.To test the end points here,register and login with your '
-                         'registered username and password.Hit the authorize button and then, Provide token to the '
-                         'secured endpoints.e.g `Bearer yourtoken`<br>Click on models to view samples of data input',
-             authorizations=authorization,
-             )
-
+api_v1 = Api(blueprint, title='StackOverflow - Lite', version='1',
+             description='An api to create a question, create an answer to a question ,get all questions and get a single ''question with its answers.To test the end points here,register and login with your ''registered username and password.Hit the authorize button and then, Provide token to the ''secured endpoints.e.g `Bearer yourtoken`<br>Click on models to view samples of data input',
+             authorizations=authorization, )
 api_home = Api(blueprint_2,
                title='StackOverflow - Lite',
                version='1',
@@ -42,12 +34,12 @@ api_home = Api(blueprint_2,
 
 app.register_blueprint(blueprint)
 
-
-
 jwt._set_error_handler_callbacks(api_v1)
+
+
 @app.errorhandler(404)
-# This method handles error 404
 def error_404(e):
+    '''This method handles error 404'''
     return jsonify({"message": "Sorry!!!The page you were looking for was not found.Kindly countercheck the url"}), 404
 
 
